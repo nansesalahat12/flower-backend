7891b73f02a1fd4301a200374c4478929e0e5c01
Nanse Salahat <nansesalahat12@gmail.com>
2025-05-31 01:20:00 +0300

    تعديل الاتصال بـ MongoDB عبر MONGO_URI من Render

--- db.js
+++ db.js
@@ -1,19 +1,16 @@
-//db.js
-
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb://localhost:27017/flowers', {
+    await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
     console.log('✅ تم الاتصال بقاعدة البيانات MongoDB بنجاح!');
   } catch (error) {
     console.error('❌ خطأ في الاتصال بقاعدة البيانات:', error.message);
-    process.exit(1); // إنهاء العملية في حالة وجود خطأ
+    process.exit(1);
   }
 };
 
 module.exports = connectDB;
-
